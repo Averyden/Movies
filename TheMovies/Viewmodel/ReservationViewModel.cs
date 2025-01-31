02982c03beac222f0ef65ced2a24c31419850386
averyden <igo408gt@gmail.com>
2025-01-31 08:41:26 +0100

    Build constructor for RVM

--- TheMovies/Viewmodel/ReservationViewModel.cs
+++ TheMovies/Viewmodel/ReservationViewModel.cs
@@ -2,6 +2,32 @@
 {
     public class ReservationViewModel
     {
+        public ReservationRepository Repository { get; set; }
+
+        private Reservation reservation;
+
+        public string CusName { get; set; }
+        public string CusMail { get; set; }
+
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public string[] Seat { get; set; }
+
+        public Reservation Reserve => reservation;
+
+        public ReservationViewModel(Reservation reservation)
+        {
+            this.reservation = reservation;
+            Amount = reservation.Amount;
+            Price = reservation.Price;
+            Seat = reservation.Seat;
+            
+            string[] CusInfo = reservation.GetCustomerInfo().Split(';');
+
+            CusName = CusInfo[0];
+            CusMail = CusInfo[1];
+        }
+
 
     }
 }
